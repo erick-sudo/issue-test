61beef13162c951283c0013368f114b6c4405f19
erick-sudo <erickochieng766@gmail.com>
2024-05-01 14:41:31 +0300

    Commit

--- netlify/edge-functions/handle-issues.js
+++ netlify/edge-functions/handle-issues.js
@@ -1,4 +1,9 @@
 export default async (event) => {
+
+  console.log("-----------------------------------------------------------------------------")
+  console.log(event)
+  console.log("-----------------------------------------------------------------------------")
+
   return new Response(JSON.stringify({ welcome: "Hellow!!" }), {
     status: 200,
     headers: {
